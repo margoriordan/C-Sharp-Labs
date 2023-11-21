e04b45e6d089b4b2a81bb017bfb4e73cfb35cc9b
margoriordan <margaret.rrdn@gmail.com>
2023-11-21 11:14:06 -0500

    Add Lab 6

---
+++ Lab 6/BankAccount.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class BankAccount
+    {
+        public BankAccount(String accountNum, decimal accountBlnc) { 
+                this.AccountBalance = accountBlnc;
+                this.AccountNumber = accountNum;
+        }
+
+        public String AccountNumber { get; }
+
+        private decimal accountBalance;
+        public decimal AccountBalance
+        {
+            get { return GetAccountBalance(); }
+            private set { accountBalance = value; }
+        }
+
+        Status transStatus;
+        public List<decimal> balances { get; private set; } = new List<decimal>();
+
+        public void Withdrawal(decimal amount)
+        {
+            decimal transAmt = accountBalance - amount;
+            
+            if (accountBalance < -100 || transAmt < -100) {
+
+                transStatus = Status.InsufficientFunds;
+                
+            } 
+            else if (amount > accountBalance && transAmt > -100m) 
+            {
+                transStatus = Status.InsufficientFunds;
+                accountBalance = transAmt - 35.75m;
+                transStatus = Status.Overdrawn;
+                balances.Add(transAmt);
+                balances.Add(accountBalance);
+            } else 
+            {
+                accountBalance = transAmt;
+                transStatus = Status.Ok;
+                balances.Add(accountBalance);
+            }
+
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount >= 10000)
+            {
+                transStatus = Status.DepositTooLarge;
+       
+            }
+            else if (amount < 10000)
+            {   
+                accountBalance += amount;
+                transStatus = Status.Ok;
+                balances.Add(accountBalance);
+            }
+        }
+
+        public enum Status
+        {
+            Ok,
+            Overdrawn,
+            InsufficientFunds,
+            DepositTooLarge
+        }
+
+        public String statusMsg = "";
+        public string GetAccountStatus(decimal accountBlc) {
+            
+            switch (transStatus)
+            {
+                case Status.Ok:
+                    statusMsg = "Transaction successful!";
+                    break;
+                case Status.Overdrawn:
+                    statusMsg = "Your account is overdrawn. Please make a deposit.";
+                    break;
+                case Status.InsufficientFunds:
+                    statusMsg = "Your account has insufficient funds for this transaction.";
+                    break;
+                case Status.DepositTooLarge:
+                    statusMsg = "Deposits must be less than 10,000. Please see teller.";
+                    break;
+            } return statusMsg;
+        
+        }
+        private decimal GetAccountBalance()
+        {
+            return this.accountBalance ;
+        }
+
+    }
+}
---
+++ Lab 6/BankAcctManager.cs
@@ -0,0 +1,95 @@
+namespace Lab_6
+{
+    public partial class BankAcctManager : Form
+    {
+        BankAccount bankAccount = new BankAccount("000302019", 1362.59m);
+
+        public BankAcctManager()
+        {
+            InitializeComponent();
+            acctBlncTxt.Text = bankAccount.AccountBalance.ToString("C");
+            acctNumTxt.Text = bankAccount.AccountNumber;
+            bankAccount.balances.Add(bankAccount.AccountBalance);
+
+        }
+
+        private void depositRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void withdrlRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            decimal amount = Decimal.Parse(amtTxt.Text);
+
+            if (depositRadio.Checked)
+            {
+                bankAccount.Deposit(amount);
+
+            }
+            else if (withdrlRadio.Checked)
+            {
+                bankAccount.Withdrawal(amount);
+
+            }
+            acctBlncTxt.Text = bankAccount.AccountBalance.ToString("C");
+            amtTxt.Text = "";
+
+            bankAccount.GetAccountStatus(bankAccount.AccountBalance);
+            statusTxt.Text = bankAccount.statusMsg.ToString();
+
+        }
+
+        private void historyBtn_Click(object sender, EventArgs e)
+        {
+            historyTxt.Text = "";
+            bankAccount.balances.Reverse();//list balance history in order starting with most recent
+            foreach (decimal balance in bankAccount.balances)
+            {
+                historyTxt.Text += balance.ToString("C");
+                historyTxt.Text += Environment.NewLine;
+            }
+            bankAccount.balances.Reverse();//reverse again to prepare list for additional balances in chron order
+
+        }
+
+        private void statusBtn_Click(object sender, EventArgs e)
+        {
+
+            statusTxt.Text = "";
+            String currentStatus = "";
+            if (bankAccount.AccountBalance > 0)
+            {
+                currentStatus = "Your account is currently okay.";
+
+            }
+            else if (bankAccount.AccountBalance < 0)
+            {
+                currentStatus = "Your account is overdrawn. Please make a deposit.";
+            }
+            else
+            {
+                currentStatus = "Your account is empty.";
+            }
+            MessageBox.Show($"{currentStatus}", "Account Status");
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void amtTxt_TextChanged(object sender, EventArgs e)
+        {
+            //clear all possible options & messages before another transaction occurs
+            depositRadio.Checked = false;
+            withdrlRadio.Checked = false;
+            statusTxt.Text = "";
+        }
+    }
+}
